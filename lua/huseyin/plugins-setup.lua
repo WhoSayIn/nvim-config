40b78badd0e23179465bc3ac0873254c3b11c510
Huseyin Keles <huseyinkeles7@hotmail.com>
2023-05-21 21:14:25 +0200

    Add cmp-cmdline

--- lua/huseyin/plugins-setup.lua
+++ lua/huseyin/plugins-setup.lua
@@ -45,9 +45,10 @@ return packer.startup(function(use)
   use("RRethy/vim-illuminate")
 
   -- autocomplete
-  use("hrsh7th/nvim-cmp")  -- completion plugin
+  use("hrsh7th/nvim-cmp")   -- completion plugin
   use("hrsh7th/cmp-buffer") -- source for text in buffer
-  use("hrsh7th/cmp-path")  -- source for file system paths
+  use("hrsh7th/cmp-path")   -- source for file system paths
+  use("hrsh7th/cmp-cmdline") -- source for file system paths
 
   -- snippets
   use("L3MON4D3/LuaSnip")            -- snippet engine
