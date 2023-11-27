164e09f66293da0ad4df243d173216df45865730
Huseyin Keles <huseyinkeles7@hotmail.com>
2023-11-27 15:31:43 +0100

    Add statuscol.nvim

--- lua/huseyin/plugins/nvim-ufo.lua
+++ lua/huseyin/plugins/nvim-ufo.lua
@@ -1,6 +1,23 @@
 return {
   "kevinhwang91/nvim-ufo",
-  dependencies = { "kevinhwang91/promise-async" },
+  -- dependencies = { "kevinhwang91/promise-async" },
+  dependencies = {
+    "kevinhwang91/promise-async",
+    {
+      "luukvbaal/statuscol.nvim",
+      config = function()
+        local builtin = require("statuscol.builtin")
+        require("statuscol").setup({
+          relculright = true,
+          segments = {
+            { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+            { text = { "%s" }, click = "v:lua.ScSa" },
+            { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+          },
+        })
+      end,
+    },
+  },
   config = function()
     local ufo = require("ufo")
     -- Using ufo provider need remapping `zR` and `zM`
