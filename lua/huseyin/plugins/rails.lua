e83671d507fcf7c15f3aa7e7a228cffa43a451dc
Huseyin Keles <huseyinkeles7@hotmail.com>
2024-01-31 00:40:01 +0100

    Add keymaps for Rails alternate commands

--- lua/huseyin/plugins/rails.lua
+++ lua/huseyin/plugins/rails.lua
@@ -3,4 +3,10 @@ return {
   dependencies = { "tpope/vim-bundler", "tpope/vim-dispatch" },
   lazy = true,
   ft = { "ruby", "eruby" },
+  keys = {
+    { "<leader>ab", ":A<CR>", mode = { "n", "x" }, desc = "Alternate - Buffer" },
+    { "<leader>av", ":AV<CR>", mode = { "n", "x" }, desc = "Alternate - Split Vertically" },
+    { "<leader>as", ":AS<CR>", mode = { "n", "x" }, desc = "Alternate - Split Horizontally" },
+    { "<leader>at", ":AT<CR>", mode = { "n", "x" }, desc = "Alternate - Tab" },
+  },
 }
