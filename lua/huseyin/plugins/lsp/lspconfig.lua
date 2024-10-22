19ac29649bb6a0dfff70468891e5700c7be7fea6
Huseyin Keles <huseyinkeles7@hotmail.com>
2024-10-22 12:08:57 +0200

    Fix descriptions for lspsaga

--- lua/huseyin/plugins/lsp/lspconfig.lua
+++ lua/huseyin/plugins/lsp/lspconfig.lua
@@ -16,60 +16,41 @@ return {
     local on_attach = function(client, bufnr)
       local opts = { noremap = true, silent = true, buffer = bufnr }
 
-      k.set(
-        "n",
-        "gf",
-        "<cmd>Lspsaga finder<CR>",
-        opts,
-        { desc = "Lspsaga: show definition, references, implementations, usages" }
-      )
-      k.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts, { desc = "Lspsaga: go to declaration" })
-      k.set(
-        "n",
-        "gd",
-        "<cmd>Lspsaga peek_definition<CR>",
-        opts,
-        { desc = "Lspsaga: see definition and make edits in window" }
-      )
-      k.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts, { desc = "Lspsaga: go to implementation" })
-      k.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts, { desc = "Lspsaga: see available code actions" })
-      k.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts, { desc = "Lspsaga: smart rename" })
-      k.set(
-        "n",
-        "<leader>D",
-        "<cmd>Lspsaga show_line_diagnostics<CR>",
-        opts,
-        { desc = "Lspsaga: show  diagnostics for line" }
-      )
-      k.set(
-        "n",
-        "<leader>d",
-        "<cmd>Lspsaga show_cursor_diagnostics<CR>",
-        opts,
-        { desc = "Lspsaga: show diagnostics for cursor" }
-      )
-      k.set(
-        "n",
-        "[d",
-        "<cmd>Lspsaga diagnostic_jump_prev<CR>",
-        opts,
-        { desc = "Lspsaga: jump to previous diagnostic in buffer" }
-      )
-      k.set(
-        "n",
-        "]d",
-        "<cmd>Lspsaga diagnostic_jump_next<CR>",
-        opts,
-        { desc = "Lspsaga: jump to next diagnostic in buffer" }
-      )
-      k.set(
-        "n",
-        "K",
-        "<cmd>Lspsaga hover_doc<CR>",
-        opts,
-        { desc = "Lspsaga: show documentation for what is under cursor" }
-      )
-      k.set("n", "<leader>o", "<cmd>Lspsaga outline<CR>", opts, { desc = "Lspsaga: see outline on right hand side" })
+      opts["desc"] = "Lspsaga: show definition, references, implementations, usages"
+      k.set("n", "gf", "<cmd>Lspsaga finder<CR>", opts)
+
+      opts["desc"] = "Lspsaga: go to declaration"
+      k.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+
+      opts["desc"] = "Lspsaga: see definition and make edits in window"
+      k.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
+
+      opts["desc"] = "Lspsaga: go to implementation"
+      k.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+
+      opts["desc"] = "Lspsaga: see available code actions"
+      k.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+
+      opts["desc"] = "Lspsaga: smart rename"
+      k.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
+
+      opts["desc"] = "Lspsaga: show  diagnostics for line"
+      k.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+
+      opts["desc"] = "Lspsaga: show diagnostics for cursor"
+      k.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+
+      opts["desc"] = "Lspsaga: jump to previous diagnostic in buffer"
+      k.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+
+      opts["desc"] = "Lspsaga: jump to next diagnostic in buffer"
+      k.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+
+      opts["desc"] = "Lspsaga: show documentation for what is under cursor"
+      k.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+
+      opts["desc"] = "Lspsaga: see outline on right hand side"
+      k.set("n", "<leader>o", "<cmd>Lspsaga outline<CR>", opts)
 
       -- typescript specific keymaps (e.g. rename file and update imports)
       if client.name == "ts_ls" then
