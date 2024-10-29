cbb01294e8638c634dedb3edf1efabe208a69e19
Huseyin Keles <huseyinkeles7@hotmail.com>
2024-10-29 22:58:26 +0100

    Remove null-ls

--- lua/huseyin/plugins/lsp/mason.lua
+++ lua/huseyin/plugins/lsp/mason.lua
@@ -2,14 +2,11 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
-    "jayp0521/mason-null-ls.nvim",
   },
   config = function()
     local mason = require("mason")
 
     local mason_lspconfig = require("mason-lspconfig")
-
-    local mason_null_ls = require("mason-null-ls")
 
     mason.setup({
       ui = {
@@ -34,27 +31,6 @@ return {
         "ts_ls",
         "ruby_lsp",
         "rubocop",
-      },
-      automatic_installation = true,
-    })
-
-    mason_null_ls.setup({
-      ensure_installed = {
-        -- JS TS
-        "prettier",
-        "stylua",
-        "eslint_d",
-
-        -- Python
-        "mypy",
-        "flake8",
-        "isort",
-        "autopep8",
-        "ruff",
-        "black",
-
-        -- Kotlin
-        "ktlint",
       },
       automatic_installation = true,
     })
