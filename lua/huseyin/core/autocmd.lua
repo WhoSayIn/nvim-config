d8567876fa198166bd952aebd5415cca708b3376
Huseyin Keles <huseyinkeles7@hotmail.com>
2025-01-20 14:30:37 +0100

    Restore cursor to file position in previous editing session

--- lua/huseyin/core/autocmd.lua
+++ lua/huseyin/core/autocmd.lua
@@ -64,3 +64,14 @@ vim.api.nvim_create_autocmd("FileType", {
     })
   end,
 })
+
+-- Restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function(args)
+    local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(args.buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.cmd('normal! g`"zz')
+    end
+  end,
+})
